c7d40b2d5378f5f8ad0df3d48d20a477e24988f2
yyb-home <yyb_home@126.com>
2019-10-22 11:06:45 +0800

    修改

---
+++ src/components/home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+      <home-header></home-header>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import HomeHeader from "./components/Header";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    HomeHeader
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+.home {
+  font-size: 24px;
+
+  .tilte {
+    &.active {
+    }
+  }
+
+  .content {
+  }
+}
+</style>
---
+++ src/components/home/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <i class="iconfont iconfanhui"></i>
+    </div>
+    <div class="header-input">
+      <i class="iconfont iconsousuo"></i>请输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">城市<i class="iconfont iconjiantou"></i>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {};
+  },
+  components: {}
+};
+</script>
+
+<style scoped lang="stylus">
+@import '~styles/varibles.styl';
+.header {
+  height: 40px;
+  width: 100%;
+  display: flex;
+  color: #fff;
+  font-size: 12px;
+  line-height: 40px;
+  text-align: center;
+  background: $bgColor;
+
+  .header-left {
+    display: inline-block;
+    flex: 0 0 50px;
+    width: 50px;
+  }
+
+  .header-input {
+    flex: 1;
+    background: #fff;
+    margin: 6px 0;
+    border-radius: 1px;
+    color: rgb(147, 153, 159);
+    line-height: 28px;
+    text-align: left;
+    padding-left:5px;
+    .iconsousuo{
+        margin-right :5px;
+        font-size:12px;
+        }
+  }
+
+  .header-right {
+    display: inline-block;
+    flex: 0 0 50px;
+    width: 50px;
+
+    .iconjiantou {
+      font-size: 8px;
+    }
+  }
+}
+</style>
